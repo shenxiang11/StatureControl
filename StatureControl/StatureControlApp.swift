237f60c9d6a6b201ed29df07ea19b8c641b0beb3
Shen <863461783@qq.com>
2024-03-14 14:23:13 +0800

    Initial Commit

---
+++ StatureControl/StatureControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatureControlApp.swift
+//  StatureControl
+//
+//  Created by 香饽饽zizizi on 2024/3/14.
+//
+
+import SwiftUI
+
+@main
+struct StatureControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
